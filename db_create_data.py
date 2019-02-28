10e37f25467a116b415a3f9b50b54f1df2798ed5
abhinayaRavi1 <aravi1@binghamton.edu>
2019-02-28 00:29:02 -0500

    Latest changes

--- db_create_data.py
+++ db_create_data.py
@@ -1,5 +1,56 @@
-import db_schema
 import mongoengine
-mongoengine.connection.connect('employee', host='localhost', port=27017)
 
-#obj1 = db_schema.EmpDetails(emp_id=10, first_name="Second", last_name="Emp", dining_hall= "App", slot_hours=["4:45pm-9pm"],no_of_hours=4.5).save()
+#mongoengine.connection.connect('employee', host='localhost', port=27017)
+
+TEST_DATA = [
+    {
+        'emp_id': 123456,
+        'first_name': 'Tom',
+        'last_name': 'Jonas',
+        'dining_hall': 'CIW',
+        'slot_hours': {"Mon": "4:45pm-9pm"},
+        'no_of_hours': 4.5
+    },
+    {
+        'emp_id': 678901,
+        'first_name':'Adam',
+        'last_name': 'Green',
+        'dining_hall': 'Hinman',
+        'slot_hours': {"Tue": "6pm-9pm"},
+        'no_of_hours': 3
+    },
+    {
+        'emp_id': 246810,
+        'first_name': 'John',
+        'last_name': 'Berg',
+        'dining_hall': 'Hinman',
+        'slot_hours': {"Wed": "10am-2pm", "Thur": "6pm-9pm"},
+        'no_of_hours': 7
+    }
+]
+
+
+#obj1 = db_schema.EmpDetails(emp_id=10, first_name="Second", last_name="Emp", dining_hall= "Hinman", slot_hours=["4:45pm-9pm"],no_of_hours=4.5).save()
+#obj2 = db_schema.EmpDetails(emp_id=10, first_name="Second", last_name="Emp", dining_hall= "Hinman", slot_hours=["4:45pm-9pm"],no_of_hours=4.5).save()
+
+
+
+# list_ids = []
+# def rand_num():
+#     return random.randint(100000, 999999)
+#
+#
+# firstName = ['John', 'Alpha', 'Beta', 'Gamma']
+# lastName = ['Gates', 'Jobs', 'Express']
+# diningHall = ['Hinman', 'App', 'CIW']
+# no_of_hours = [2, 4, 6, 2.5, 20]
+# list_ids.append(rand_num())
+# print(list_ids)
+#
+# for x in range(0,5):
+#     employee = {
+#         'emp_id':
+#         'first_name': firstName[random(0,(len(firstName)-1)] ,
+#         'last_name':lastName[random(0,len(firstName)-1)] ,
+#         'dining_hall':
+#     }
--- shift.py
+++ shift.py
@@ -1,20 +1,55 @@
 import db_create_data
 import db_schema
+import pymongo
+from pprint import pprint
 
+TEST_DATA = [
+    {
+        'emp_id': 123456,
+        'first_name': 'Tom',
+        'last_name': 'Jonas',
+        'dining_hall': 'CIW',
+        'slot_hours': {"Mon": "4:45pm-9pm"},
+        'no_of_hours': 4.5
+    },
+    {
+        'emp_id': 678901,
+        'first_name':'Adam',
+        'last_name': 'Green',
+        'dining_hall': 'Hinman',
+        'slot_hours': {"Tue": "6pm-9pm"},
+        'no_of_hours': 3
+    },
+    {
+        'emp_id': 246810,
+        'first_name': 'John',
+        'last_name': 'Berg',
+        'dining_hall': 'Hinman',
+        'slot_hours': {"Wed": "10am-2pm", "Thur": "6pm-9pm"},
+        'no_of_hours': 7
+    }
+]
 
-# Employee details
-def display_emp_details():
-    print('Employee Details')
-    for user in db_schema.EmpDetails.objects:
-        print(str(user.emp_id)+" " + user.first_name+" "+user.last_name+" "+user.dining_hall+" "+" ".join(user.slot_hours)+" " + str(user.no_of_hours))
+# # Employee details
+# def display_emp_details():
+#     print('Employee Details')
+#     for user in db_schema.EmpDetails.objects:
+#         print(str(user.emp_id)+" " + user.first_name+" "+user.last_name+" "+user.dining_hall+" "+" ".join(user.slot_hours)+" " + str(user.no_of_hours))
 
+#Employee details
+def display_emp_details(emp_db):
+    print("Employee Details:")
+    cursor = emp_db.find()
+    for doc in cursor:
+        pprint(doc)
 
 # Give cover
 def give_cover():
     #User id and slot to give - IP
     #Backend - mark that slot
     emp_id, slot_time = input("Enter employee id and slot to give").split()
-
+    print(emp_id, slot_time)
+    
 
 # Take cover
 def take_cover():
@@ -22,7 +57,12 @@ def take_cover():
 
 def main():
 
-    db_create_data
+    uri = "mongodb://localhost:27017/"
+    client = pymongo.MongoClient(uri)
+    db = client["employee"]
+    emp_db = db["empDetails"]
+    emp_db.insert_many(TEST_DATA)
+    
     choice = -1
     # Menu Based interface
     while choice != 4:
@@ -33,9 +73,9 @@ def main():
         print("4.Exit")
         choice = int(input("Enter choice"))
         if choice == 1:
-            display_emp_details()
+            display_emp_details(emp_db)
         elif choice == 2:
-            pass
+            give_cover()
         elif choice == 3:
             pass
         elif choice == 4:
